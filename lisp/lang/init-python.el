cb5b86afd84a7aa3bc51acd11edbb3ef7809b4f6
cabins <kong_lingcun@hotmail.com>
2021-10-13 12:04:35 +0800

    fix python isrot bug

--- lisp/lang/init-python.el
+++ lisp/lang/init-python.el
@@ -3,38 +3,36 @@
 
 ;;; Code:
 
-(defmacro check-run-execute (exec-file &rest body)
+;;;###autoload
+(defun check-run-command (command arg-string)
   "Find the EXEC-FILE and run the BODY."
 
-  `(if (not (executable-find ,exec-file))
-       (message "[ERROR]: <%s> not found!" ,exec-file)
-     ,@body))
+  (if (not (executable-find command))
+      (message "[ERROR]: <%s> not found!" command)
+    (save-buffer)
+    (shell-command (format "%s %s" command arg-string))
+    (revert-buffer t t t)))
 
 ;;;###autoload
 (defun python-isort ()
   "Sort the imports with isort."
   (interactive)
-  (check-run-execute "isort"
-		     (shell-command-on-region
-		      (point-min) (point-max)
-		      "isort --atomic --profile=black -"
-		      (current-buffer) t)))
+  (check-run-command "isort"
+		     (format "--atomic --profile=black %s"
+			     (buffer-file-name))))
 
 ;;;###autoload
 (defun python-remove-all-unused-imports ()
   "Remove all the unused imports, do NOT use pyimport, as it has bugs.
 eg.from datetime import datetime."
   (interactive)
-  (check-run-execute "autoflake"
-		     (shell-command
-		      (format "autoflake -i --remove-all-unused-imports %s"
-			      (buffer-file-name)))
-		     (revert-buffer t t t)))
+  (check-run-command "autoflake"
+		     (format "-i --remove-all-unused-imports %s"
+			     (buffer-file-name))))
 
 (add-hook
  'python-mode-hook
  (lambda ()
-   (add-hook 'before-save-hook #'python-isort nil t)
    (define-key python-mode-map (kbd "C-c p s") 'python-isort)
    (define-key python-mode-map (kbd "C-c p r") 'python-remove-all-unused-imports)))
 
