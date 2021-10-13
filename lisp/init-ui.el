b5f47c879d4eb5533093ddaa0cdc0613e05ff272
cabins <kong_lingcun@hotmail.com>
2021-10-13 08:46:07 +0800

    fix font rescale issue on Windows(to be tested on macOS)

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -17,18 +17,23 @@
 (defun tenon/setup-font ()
   "Font setup."
 
-  (let* ((enfonts '("Cascadia Code" "Source Code Pro" "Consolas" "Ubuntu Mono" "Monaco"))
-	 (cnfonts '("STKaiti" "华文楷体" "STHeiti" "华文黑体" "微软雅黑" "文泉驿微米黑"))
+  (let* ((enfonts '("Cascadia Code" "Source Code Pro" "Consolas" "Monaco"))
+	 (cnfonts '("STKaiti" "华文楷体" "STHeiti" "华文黑体" "微软雅黑"))
 	 (cnfont (font-available cnfonts))
 	 (enfont (font-available enfonts)))
     (if enfont
 	(set-face-attribute 'default nil
 			    :font (format "%s-%d" enfont 10.0))
       (message "Failed to set default font."))
-    (if cnfont
-	(set-fontset-font "fontset-default" 'han
-			  (font-spec :family cnfont
-				     :size (if (eq system-type 'darwin) 12.5 11.0)))
+    (if cnfont (progn
+		 (set-fontset-font "fontset-default" 'han
+				   (font-spec :family cnfont))
+		 (setq face-font-rescale-alist
+		       '(("STKaiti" . 1.2)
+			 ("华文楷体" . 1.2)
+			 ("STHeiti" . 1.2)
+			 ("华文黑体" . 1.2)
+			 ("微软雅黑" . 1.2))))
       (message "Failed to set Chinese font."))))
 
 (tenon/setup-font)
