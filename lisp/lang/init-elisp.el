396c308979eb6261766c93da3b5d44869bdaf3d2
cabins <kong_lingcun@hotmail.com>
2021-10-16 21:19:54 +0800

    add packages

--- lisp/lang/init-elisp.el
+++ lisp/lang/init-elisp.el
@@ -4,14 +4,19 @@
 ;;; Code:
 
 ;; Lisp
-(use-package paredit
-  :init
-  (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode))
+;; (use-package paredit
+;;   :init
+;;   (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode))
 
 ;; lispy (iedit, hydra as dependency), if you like vim style
 ;; (use-package lispy
 ;;   :init
 ;;   (add-hook 'emacs-lisp-mode-hook 'lispy-mode))
+
+;; maybe smartparens is enough, and NO C-, binding
+(use-package smartparens
+  :init
+  (add-hook 'prog-mode-hook 'smartparens-strict-mode))
 
 (provide 'init-elisp)
 ;; Local Variables:
