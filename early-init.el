bdb5918a8fc11a3a2434089cca37a0a09bb02528
cabins <kong_lingcun@hotmail.com>
2021-10-25 09:30:27 +0800

    fix auto save issue

--- early-init.el
+++ early-init.el
@@ -3,9 +3,9 @@
 ;; Runs before package and UI initializetion happens.
 ;;; Code:
 
-;; Package initialize occurs automatically, before `user-init-file' is
-;; loaded, but after `early-init-file'. We handle package
-;; initialization, so we must prevent Emacs from doing it early!
+;; Package initialize occurs automatically, before `user-init-file' is loaded
+;; but after `early-init-file'. If you want to handle package initialization,
+;; you can prevent Emacs from doing it early by uncomment next line!
 ;; (setq package-enable-at-startup nil)
 
 ;; Do not resize the frame at this early stage.
