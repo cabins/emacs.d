d644ad414047ec714d62fdc1dbffbc881bc1e6c3
cabins <kong_lingcun@foxmail.com>
2022-02-15 15:03:41 +0800

    update

--- lisp/init-java.el
+++ lisp/init-java.el
@@ -5,13 +5,18 @@
 
 ;; Java
 
-(add-hook 'java-mode-hook
-          (lambda ()
-            (let* ((jarpath "eglot/server/java/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar")
-                   (eglot-server-java (expand-file-name jarpath user-emacs-directory)))
-              (if (file-exists-p eglot-server-java)
-                  (setenv "CLASSPATH"
-                          (concat (getenv "CLASSPATH") path-separator eglot-server-java))))))
+;; define the jdt-language-server manually
+(defun eglot-server-java-setup ()
+  (setq eglot-server-java
+        (expand-file-name
+         "eglot/server/java/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar"
+         user-emacs-directory))
+
+  (if (file-exists-p eglot-server-java)
+      (setenv "CLASSPATH"
+              (concat (getenv "CLASSPATH") path-separator eglot-server-java))))
+
+(add-hook 'java-mode-hook #'eglot-server-java-setup)
 
 (provide 'init-java)
 
