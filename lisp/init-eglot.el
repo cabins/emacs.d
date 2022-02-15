0b4083f28cbb92c08bb3cb1e3f9f2fb131d3605d
cabins <kong_lingcun@foxmail.com>
2022-02-15 15:03:41 +0800

    update

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -10,11 +10,12 @@
          ("C-c e q" . #'eglot-code-action-quickfix))
   :config
   ;; (setq eglot-ignored-server-capabilities '(:documentHighlightProvider))
-  (defun eglot-actions-before-save()
-    (add-hook 'before-save-hook (lambda ()
-                                  (call-interactively #'eglot-format)
-                                  (call-interactively #'eglot-code-action-organize-imports))))
   (add-to-list 'eglot-server-programs '(web-mode "vls"))
+  (defun eglot-actions-before-save()
+    (add-hook 'before-save-hook
+              (lambda ()
+                (call-interactively #'eglot-format)
+                (call-interactively #'eglot-code-action-organize-imports))))
   (add-hook 'eglot--managed-mode-hook #'eglot-actions-before-save))
 
 (provide 'init-eglot)
