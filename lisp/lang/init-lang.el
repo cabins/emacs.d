760154caa6397a8bf8047cb8d1b63b261ccd689b
unknown <11069956@vivo.com>
2022-02-22 12:29:37 +0800

    enable lsp-mode

--- lisp/lang/init-lang.el
+++ lisp/lang/init-lang.el
@@ -8,8 +8,8 @@
 ;;; Code:
 
 ;; language server
-(require 'init-eglot) ; eglot
-;; (require 'init-lsp)   ; lsp, enable this line if you like lsp-mode and disable eglot line
+;; (require 'init-eglot) ; eglot
+(require 'init-lsp)   ; lsp, enable this line if you like lsp-mode and disable eglot line
 
 ;; specific languages
 (require 'init-lang-go)
