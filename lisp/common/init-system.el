01e7ea13a1d96724cf3f1d7de8d3f8bb8d909196
cabins <kong_lingcun@foxmail.com>
2022-05-26 10:57:57 +0800

    less useless feature

--- lisp/common/init-system.el
+++ lisp/common/init-system.el
@@ -12,7 +12,8 @@
 ;;; system coding
 ;; although others may add many other settings here,
 ;; but I think the next line is enough
-(prefer-coding-system 'utf-8)
+;;(prefer-coding-system 'utf-8)
+(set-language-environment "UTF-8")
 
 ;;; emacs settings
 (setq auto-window-vscroll nil
