3589ce6e7ac0d31a0fdcdc7114ff5e939560846d
cabins <kong_lingcun@foxmail.com>
2022-06-27 08:58:39 +0800

    turn input method by default

---
+++ lisp/extras/init-input-methods.el
@@ -0,0 +1,13 @@
+;;; init-input-methods --- Settings for Flypy & Wubi98 input method by Quail.
+;;; Commentary:
+;;; Code:
+
+;; 小鹤音形输入法
+(require 'init-chinese-flypy)
+
+;; 98五笔输入法
+(require 'init-chinese-wb98)
+
+(provide 'init-input-methods)
+
+;;; init-input-methods.el end here.
