27dbf59ca355c986bac133664231a9ef94d0bd10
cabins <kong_lingcun@foxmail.com>
2022-07-07 08:33:20 +0800

    add init-utils-interactive

---
+++ lisp/extras/init-utils-interactive.el
@@ -0,0 +1,36 @@
+;;; init-utils-interactive --- some interactively called functions
+;;; Commentary:
+;;; Code
+
+;; ********** some useful functions / alias **********
+
+;;;###autoload
+(defun preferences()
+  "Nothing, but alias for `crux-find-user-init-file', inspired by VSCode."
+
+  (interactive)
+  (funcall-interactively 'crux-find-user-init-file)
+  (funcall-interactively 'delete-other-windows))
+
+;;;###autoload
+(defun preferences-custom()
+  "Nothing, but alias for `crux-find-user-custom-file', inspired by VSCode."
+
+  (interactive)
+  (funcall-interactively 'crux-find-user-custom-file)
+  (funcall-interactively 'delete-other-windows))
+
+;;;###autoload
+(defun check-upgrade()
+  "Checking upgrade of packages."
+  (interactive)
+  (call-interactively 'package-refresh-contents)
+  (call-interactively 'package-update-all))
+
+;; ********** keybindings **********
+(global-set-key (kbd "C-,") 'preferences)
+(global-set-key (kbd "<M-RET>") #'toggle-frame-maximized)
+
+(provide 'init-utils-interactive)
+
+;;; init-utils-interactive.el ends here
