edf0877d2385a20e5087271cbd4e24b9d30414aa
KongLingCun <11069956@vivo.com>
2022-09-14 14:54:55 +0800

    new font settings and themes

--- lisp/common/init-fn.el
+++ lisp/common/init-fn.el
@@ -5,36 +5,36 @@
 ;;; Code:
 (require 'subr-x)
 
-(defun cabins/available-font (font-list)
-  "Get the first available font from FONT-LIST."
+(defun font-installed-p (font-name)
+  "Check if font with FONT-NAME is available."
+  (find-font (font-spec :name font-name)))
 
-  (cl-loop for font in font-list
-           when (member font (font-family-list))
-           return font))
-
-;;;###autoload
-(defun cabins/font-setup-charset(font-list charset-list)
-  "Settings for CHARSET-LIST with fonts from FONT-LIST."
-
-  (let ((font (cabins/available-font font-list)))
-    (when font
-      (dolist (charset charset-list)
-	    (set-fontset-font t charset font))
-      (add-to-list 'face-font-rescale-alist `(,font . 1.2)))))
-
-;;;###autoload
 (defun cabins/font-setup ()
   "Font setup."
 
   (interactive)
-  ;; Default font settings
-  (dolist (face '(default fixed-pitch fixed-pitch-serif variable-pitch))
-	(set-face-attribute face nil :family (cabins/available-font '("Cascadia Code PL" "Consolas" "Menlo" "Monaco" "Ubuntu Mono" "Courier"))))
-  ;; Chinese font settings
-  (cabins/font-setup-charset '("等线" "苹方" "微软雅黑" "文泉译微米黑" "Noto Sans SC") '(han))
-  ;; Emoji font settings
-  (cabins/font-setup-charset '("Apple Color Emoji" "Noto Color Emoji" "Segoe UI Emoji" "Symbola" "Symbol") '(emoji)))
-(add-hook 'after-init-hook #'cabins/font-setup)
+  (when (display-graphic-p)
+    ;; Default font
+    (cl-loop for font in '("Cascadia Code" "Fira Code" "Jetbrains Mono" "SF Mono" "Hack" "Source Code Pro" "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
+             when (font-installed-p font)
+             return (set-face-attribute 'default nil :family font))
+
+    ;; Unicode characters
+    (cl-loop for font in '("Segoe UI Symbol" "Symbola" "Symbol")
+             when (font-installed-p font)
+             return (set-fontset-font t 'unicode font nil 'prepend))
+
+    ;; Emoji
+    (cl-loop for font in '("Noto Color Emoji" "Apple Color Emoji")
+             when (font-installed-p font)
+             return (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend))
+
+    ;; Chinese characters
+    (cl-loop for font in '("WenQuanYi Micro Hei" "PingFang SC" "Microsoft Yahei UI" "Microsoft Yahei" "STFangsong")
+             when (font-installed-p font)
+             return (progn
+                      (setq face-font-rescale-alist `((,font . 1.2)))
+                      (set-fontset-font t '(#x4e00 . #x9fff) (font-spec :family font))))))
 
 ;;;###autoload
 (defun tenon--cleaner-ui ()
@@ -63,44 +63,29 @@
            when (member theme (custom-available-themes))
            return theme))
 
-;;;###autoload
-(defun cabins/default-light-theme ()
-  "Get the default built-in light theme."
+(defun cabins/os-dark-mode()
+  "Check the os dark mode, only support Windows for now."
 
-  (let ((theme-list '(modus-operandi
-                      leuven
-                      tsdh-light
-                      tango
-                      whiteboard)))
-    (cabins/available-theme theme-list)))
-
-;;;###autoload
-(defun cabins/default-dark-theme ()
-  "Get the default built-in dark theme."
-
-  (let ((theme-list '(modus-vivendi
-                      leuven-dark
-                      tsdh-dark
-                      tango-dark
-                      wombat
-                      dichromacy)))
-    (cabins/available-theme theme-list)))
-
-;;;###autoload
-(defun cabins/set-theme-on-windows ()
-  "Set theme on Windows 10 based on system dark mode."
-
-  (interactive)
   (when (memq system-type '(ms-dos windows-nt cygwin))
-    (if (eq 'modus-operandi (cabins/default-light-theme))
-        (setq modus-themes-mode-line '(borderless (padding . 4) (height . 0.9)))
-      )
     (let* ((cmd "powershell (Get-ItemProperty -Path HKCU:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize -Name AppsUseLightTheme).AppsUseLightTheme")
            (mode (string-trim (shell-command-to-string cmd))))
-      (if (equal mode "1")
-          (load-theme (cabins/default-light-theme) t)
-        (load-theme (cabins/default-dark-theme) t)))))
-(add-hook 'after-init-hook #'cabins/set-theme-on-windows)
+      (if (equal mode "1") t nil)))
+  ;; TODO support for macOS
+  ;; TODO support for linux, GNOME only
+  )
+
+(defun cabins/load-theme()
+  "Load theme, Auto change color scheme according to system dark mode on Windows."
+
+  (interactive)
+  (let ((light-theme (cabins/available-theme '(modus-operandi leuven tsdh-light tango whiteboard)))
+        (dark-theme (cabins/available-theme '(modus-vivendi leuven-dark tsdh-dark tango-dark wombat dichromacy))))
+
+    (if (cabins/os-dark-mode)
+        (load-theme light-theme t)
+      (load-theme dark-theme t))))
+
+(add-hook 'frame-after-make-frame 'cabins/load-theme)
 
 (provide 'init-fn)
 ;;; init-fn.el ends here
