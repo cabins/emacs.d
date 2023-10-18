301c69f3c023e3525e1c1ca9938506a3032fe8b0
cabins <cabins.code@gmail.com>
2023-10-18 11:42:29 +0800

    disable menubar

--- early-init.el
+++ early-init.el
@@ -22,10 +22,9 @@
 (setq package-enable-at-startup nil)
 
 ;; Clean GUI
-(when (fboundp 'tool-bar-mode)
-  (tool-bar-mode -1))
-(when (fboundp 'scroll-bar-mode)
-  (scroll-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
 (provide 'early-init)
 
